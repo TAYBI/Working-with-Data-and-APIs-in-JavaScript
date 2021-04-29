2d52ff7a42dddc0ca33c7d5c75a9b59b3d85c537
TAYBI <taybib4@gmail.com>
2021-04-29 11:11:45 +0000

    post data to the server using fetch function

--- module 2/index.js
+++ module 2/index.js
@@ -2,3 +2,11 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("lisning"));
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+// POST method route
+app.post("/api", (req, res) => {
+  // req :  everything
+  // res: send things back to the client
+  console.log(req.body);
+});
