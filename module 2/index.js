ac86c76ec5e82a06adaebf05fe8821367b0bae5e
TAYBI <taybib4@gmail.com>
2021-04-29 12:31:03 +0000

    adding submit button to send the data and savet to an array variable

--- module 2/index.js
+++ module 2/index.js
@@ -4,13 +4,15 @@ app.listen(3000, () => console.log("lisning"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+const data = [];
+
 // POST method route
 app.post("/api", (req, res) => {
   // req :  everything
   // res: send things back to the client
-  console.log(req.body);
+  data.push(req.body);
   res.json({
     status: "success",
-    data: req.body,
+    data,
   });
 });
