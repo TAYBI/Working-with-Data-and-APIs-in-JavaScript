0e58753a5b7b1986b9b8af2171f483a34a9585fd
TAYBI <taybib4@gmail.com>
2021-04-30 13:48:38 +0000

    save into NeDB databade & adding user name

--- module 2/index.js
+++ module 2/index.js
@@ -1,29 +1,24 @@
 const express = require("express");
 const fs = require("fs");
+const Datastore = require("nedb");
+
 const app = express();
 
 app.listen(3000, () => console.log("lisning"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
-const data = [];
-const fileName = "data.csv";
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 // POST method route
 app.post("/api", (req, res) => {
   // req :  everything
   // res: send things back to the client
 
-  fs.appendFile(fileName, `\n${req.body.lat},${req.body.lon}`, (err) => {
-    if (err) throw err;
-  });
-
-  let tempData = fs.readFileSync(fileName, "utf8").split("\n").splice(1);
-
-  tempData.forEach((element) => {
-    const lat = element.split(",")[0];
-    const lon = element.split(",")[1];
-    data.push({ lat, lon });
-  });
+  const timestamp = Date.now();
+  const data = { ...req.body, timestamp };
+  database.insert(data);
 
   res.json({
     status: "success",
