366e1e0c4d0b8adbd88bc1e10b1a9a26efb77efd
TAYBI <taybib4@gmail.com>
2021-04-30 17:59:16 +0000

    adding get route to the server & disply it into att.html file

--- module 2/index.js
+++ module 2/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const Datastore = require("nedb");
-
 const app = express();
-
 app.listen(3000, () => console.log("lisning"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
@@ -11,19 +8,22 @@ app.use(express.json({ limit: "1mb" }));
 const database = new Datastore("database.db");
 database.loadDatabase();
 
+// GET method route
+app.get("/api", (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.json(data);
+  });
+});
+
 // POST method route
 app.post("/api", (req, res) => {
   // req :  everything
   // res: send things back to the client
-
   const timestamp = Date.now();
   const data = { ...req.body, timestamp };
   database.insert(data);
-
-  res.json({
-    status: "success",
-    data,
-  });
-
-  data.length = 0;
+  res.json(data);
 });
